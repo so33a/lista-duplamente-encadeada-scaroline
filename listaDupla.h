791afb7d764a2bf946849e61ed69d991786a0610
scaroline <ssouza@alunos.utfpr.edu.br>
2016-08-28 22:34:30 -0300

    Update listaDupla.h

--- listaDupla.h
+++ listaDupla.h
@@ -16,14 +16,18 @@ typedef struct {
 ListaDupla inicializa();
 link novoNo (int item, link prev, link next);
 link buscar(ListaDupla l, int item);
+link encontrado(ListaDupla1);
 void insereDepois (ListaDupla l, link x, link t);
 void insereAntes (ListaDupla l, link x, link t);
 link removeNo (link x);
 void imprime(ListaDupla l);
 void imprimeReverso(ListaDupla l);
 void destroiLista(ListaDupla l);
+void ordena();
 
-link encontrado(Lista Dupla1, link Menor);
+
+link encontrado(ListaDupla1);
+link Menor;
 link t = l->head;
 while(t!=l->z);
 if(t->item < Menor);
@@ -31,4 +35,18 @@ Menor = t -> item;
 Menor = t;
 }
 return = Menor;
+
+void ordenar(ListaDupla1)
+Link aux,aux2;
+aux=item;
+for(aux=item;aux!=NULL;aux=aux->next){
+aux2=(ListaDupla1*)malloc(1*sizeof(ListaDupla1));
+if(aux -> t-> aux -> next -> t){
+aux2->t = aux->t;
+aux->t = aux->next->t;
+aux->next->t = aux2->t;
+}
+free(aux2);
+} 
+
 #endif
